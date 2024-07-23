7a8b51461823401a9447298ad51de9650c4c1629
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-07-23 22:25:07 +0200

    add others depandance

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pandas as pd
 def fetch_arxiv_titles(category="cs.AI",max_results = 20):
     url = f'http://export.arxiv.org/api/query?search_query=cat:{category}&start=0&max_results={max_results}'
     page = requests.get(url)
-    soup = BeautifulSoup(page.content,"lxml")
+    soup = BeautifulSoup(page.content,features="xml")
     titles = [entry.title.text for entry in soup.find_all("entry")]
     return titles
 
