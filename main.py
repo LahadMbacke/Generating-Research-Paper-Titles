aeb585b9e81a2e3637cf2d0ac4b5552b9e4fb55b
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-07-24 14:51:33 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-from scraping import fetch_arxiv_titles
-from clean_data import clean_title
 import torch
-import pandas as pd
 
 
-# scraping arxiv
-titles = fetch_arxiv_titles()
-df = pd.DataFrame(titles,columns=["titles"])
-df.to_csv("arxiv_titles.csv",index=False)
+def generate_title(prompt, model,tokenizer, max_length=50,do_sample=True,):
+    input_ids = tokenizer.encode(prompt, return_tensors='pt')
+    attention_mask = torch.ones(input_ids.shape,dtype=torch.long)
+    output = model.generate(
+        input_ids,
+        attention_mask=attention_mask,
+        max_length=max_length,
+        # temperature=temperature,
+        do_sample=do_sample,
+        top_k=50,
+        top_p=0.95,
+        pad_token_id=tokenizer.pad_token_id,
+        num_return_sequences=1 
+    )
+    decode_output = tokenizer.decode(output[0], skip_special_tokens= True).strip()
+    title = decode_output.split("\n")
+    return title[0].strip()
 
-# clean data
-df = pd.read_csv("arxiv_titles.csv")
-df['titles'] = df['titles'].apply(clean_title)
-df.to_csv('cleaned_arxiv_titles.csv', index=False)
 
 
-# load model finetuning
-output_dir = "./fine_tuned_gpt2_generateTitle"
-model = GPT2LMHeadModel.from_pretrained(output_dir)
-tokenizer = GPT2Tokenizer.from_pretrained(output_dir)
+  # Test
+if __name__=="__main__":
+    from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
-def generate_title(prompt, model,tokenizer, max_length=50):
-    input = tokenizer.encode(prompt, return_tensors='pt')
-    attention_mask = torch.ones(input.shape,dtype=torch.long)
-    output = model.generate(input, max_length=max_length,attention_mask=attention_mask,pad_token_id=tokenizer.pad_token_id, 
-                            num_return_sequences=1, do_sample=True,top_k=50, top_p=0.95)
-    return tokenizer.decode(output[0], skip_special_tokens= True)
+    # load model finetuning
+    output_dir = "./fine_tuned_gpt2_generateTitle"
+    model = GPT2LMHeadModel.from_pretrained(output_dir)
+    tokenizer = GPT2Tokenizer.from_pretrained(output_dir)
 
-prompt = "Quantum Computing"
-title = generate_title(prompt, model, tokenizer)
-print(f"Generated Title: {title}")
+    prompt = "Machine Learning"
+    title = generate_title(prompt, model, tokenizer)
+    print(f"Generated Title: {title}")
