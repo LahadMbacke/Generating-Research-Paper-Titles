80922750127fb733102d5711e69c29937ce82e9f
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-07-24 14:10:44 +0200

    load model and test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from finetuning import model,tokenizer
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from scraping import fetch_arxiv_titles
 from clean_data import clean_title
 import torch
@@ -16,15 +16,18 @@ df['titles'] = df['titles'].apply(clean_title)
 df.to_csv('cleaned_arxiv_titles.csv', index=False)
 
 
+# load model finetuning
+output_dir = "./fine_tuned_gpt2_generateTitle"
+model = GPT2LMHeadModel.from_pretrained(output_dir)
+tokenizer = GPT2Tokenizer.from_pretrained(output_dir)
 
-
-def generate_title(prompt, model,tokenizer, max_length=100):
+def generate_title(prompt, model,tokenizer, max_length=50):
     input = tokenizer.encode(prompt, return_tensors='pt')
     attention_mask = torch.ones(input.shape,dtype=torch.long)
-    output = model.generate(input, max_length=max_length,attention_mask=attention_mask,pad_token_id=tokenizer.pad_token_id, num_return_sequences=1)
+    output = model.generate(input, max_length=max_length,attention_mask=attention_mask,pad_token_id=tokenizer.pad_token_id, 
+                            num_return_sequences=1, do_sample=True,top_k=50, top_p=0.95)
     return tokenizer.decode(output[0], skip_special_tokens= True)
 
-prompt = "RAG"
-title = generate_title(prompt,model,tokenizer)
-
-print(title)
+prompt = "Quantum Computing"
+title = generate_title(prompt, model, tokenizer)
+print(f"Generated Title: {title}")
